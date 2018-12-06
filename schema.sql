e5ee2a3231bb73b5a346a82c4558fed0461acd79
mbgoseco <mbgoseco@gmail.com>
2018-12-06 13:25:43 -0800

    completed feature 1 (SQL db) for location, weather, yelp, movies

--- schema.sql
+++ schema.sql
@@ -1,4 +1,6 @@
+DROP TABLE IF EXISTS movies;
 DROP TABLE IF EXISTS weathers;
+DROP TABLE IF EXISTS restaurants;
 DROP TABLE IF EXISTS locations;
 
 CREATE TABLE locations (
@@ -13,6 +15,29 @@ CREATE TABLE weathers (
   id SERIAL PRIMARY KEY,
   forecast VARCHAR(255),
   time VARCHAR(255),
+  location_id INTEGER NOT NULL,
+  FOREIGN KEY (location_id) REFERENCES locations (id)
+);
+
+CREATE TABLE restaurants (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  image_url VARCHAR(255),
+  price VARCHAR(255),
+  rating NUMERIC(4, 2),
+  url VARCHAR(255),
+  location_id INTEGER NOT NULL,
+  FOREIGN KEY (location_id) REFERENCES locations (id)
+);
+
+CREATE TABLE movies (
+  title VARCHAR(255),
+  overview VARCHAR(255),
+  average_votes NUMERIC(5, 3),
+  total_votes NUMERIC(7),
+  image_url VARCHAR(255),
+  popularity NUMERIC(5, 3),
+  released_on VARCHAR(255),
   location_id INTEGER NOT NULL,
   FOREIGN KEY (location_id) REFERENCES locations (id)
 );
